a9ddac51da63cd2264e225bef53f37715b3cf831
Cefn Hoile <github.com@cefn.com>
2019-08-11 10:04:45 +0100

    Unit testing for entities in and out of LowStore

--- test/unit/lowstore.test.js
+++ test/unit/lowstore.test.js
@@ -1,61 +1,57 @@
-const { createDb, deleteDb, parseDb } = require("../lib/dbscaffold")
+const Memory = require("lowdb/adapters/Memory")
+const {
+  createFileDb,
+  deleteFileDb,
+  parseFileDb,
+  createMemoryDb,
+  createMemoryStore
+} = require("./lowstoreharness")
 const { loadDb, LowStore } = require("../../lib/lowstore")
 
-beforeAll(() => {
-  deleteDb()
-})
-
-test("Queries load values from db file", () => {
-  let db = createDb({ fieldName: "fieldValue" })
-  expect(db.get("fieldName").value()).toEqual("fieldValue")
-})
-
 test("Lodash transformation chain followed by write is reflected in db file", () => {
-  let db = createDb({ fieldName: "fieldValue" })
+  const db = createFileDb({ fieldName: "fieldValue" })
   db.set("fieldName", "otherValue").write()
-  expect(parseDb()).toEqual({ fieldName: "otherValue" })
+  expect(parseFileDb()).toEqual({ fieldName: "otherValue" })
+  deleteFileDb()
 })
 
 test("entitiesByType lists entities of a type", () => {
-  let db = createDb({
+  const store = createMemoryStore({
     song: [
       { id: "0", title: "Baby Shark" },
       { id: "1", title: "Raining Tacos" }
     ]
   })
-  let store = LowStore(db)
-  let songs = store.entitiesByType("song")
-  expect(songs).toEqual([
+  const songs = store.iterateByType("song")
+  expect([...songs]).toEqual([
     { id: "0", title: "Baby Shark" },
     { id: "1", title: "Raining Tacos" }
   ])
 })
 
 test("entityResolverFactory creates GraphQL resolver listing entities for a type", () => {
-  const db = createDb({
+  const store = createMemoryStore({
     song: [
       { id: "0", title: "Baby Shark" },
       { id: "1", title: "Raining Tacos" }
     ]
   })
-  const store = LowStore(db)
   const resolver = store.entityResolverFactory("song")
   const { parent, args, context, info } = {} //all deliberately undefined
   const songs = resolver(parent, args, context, info)
-  expect(songs).toEqual([
+  expect([...songs]).toEqual([
     { id: "0", title: "Baby Shark" },
     { id: "1", title: "Raining Tacos" }
   ])
 })
 
-test("entityById can retrieve a single typed entity", () => {
-  let db = createDb({
+test("getById can retrieve a single typed entity", () => {
+  const store = createMemoryStore({
     song: [
       { id: "0", title: "Baby Shark" },
       { id: "1", title: "Raining Tacos" }
     ]
   })
-  let store = LowStore(db)
-  let song = store.entityById("song", "1")
+  let song = store.getById("song", "1")
   expect(song).toEqual({ id: "1", title: "Raining Tacos" })
 })
---
+++ test/unit/lowstoreharness.js
@@ -0,0 +1,40 @@
+const { writeFileSync, readFileSync, unlinkSync } = require("fs")
+const lowdb = require("lowdb")
+const Memory = require("lowdb/adapters/Memory")
+const { loadDb, LowStore } = require("../../lib/lowstore")
+
+const defaultDbPath = "/tmp/db.json"
+
+/** Remove db JSON file from disk */
+function deleteFileDb(testDbPath = defaultDbPath) {
+  unlinkSync(testDbPath)
+}
+
+/** Create db JSON file on disk */
+function createFileDb(obj, testDbPath = defaultDbPath) {
+  writeFileSync(testDbPath, JSON.stringify(obj))
+  return loadDb(testDbPath)
+}
+
+/** Parse the JSON file direct from disk without lowstore */
+function parseFileDb(testDbPath = defaultDbPath) {
+  return JSON.parse(readFileSync(testDbPath, "utf-8"))
+}
+
+/** Provide a db having the specified content, for which write HAS NO EFFECT */
+function createMemoryDb(data) {
+  return lowdb(new Memory()).defaults(data)
+}
+
+function createMemoryStore(data) {
+  const db = createMemoryDb(data)
+  return new LowStore(db)
+}
+
+module.exports = {
+  deleteFileDb,
+  createFileDb,
+  parseFileDb,
+  createMemoryDb,
+  createMemoryStore
+}
