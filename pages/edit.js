49cb55a0094102ef892e3c1b5849fabbe9ffce88
Cefn Hoile <github.com@cefn.com>
2019-08-19 18:56:25 +0100

    Configured edit page to load schemas from a server-side endpoint

--- pages/edit.js
+++ pages/edit.js
@@ -1,28 +1,35 @@
 import React, { Component } from "react"
 import { render } from "react-dom"
 import Form from "react-jsonschema-form"
+const fetch = require("isomorphic-unfetch")
+
+async function fetchSchema(typeName) {
+  const endpoint = `http://localhost:3000/schema/${typeName}`
+  const res = await fetch(endpoint, {
+    method: "GET",
+    headers: {
+      Accept: "application/json"
+    }
+  })
+  return res.json()
+}
 
 const log = type => console.log.bind(console, type)
 
-const schema = {
-  title: "Todo",
-  type: "object",
-  required: ["title"],
-  properties: {
-    title: { type: "string", title: "Title", default: "A new task" },
-    done: { type: "boolean", title: "Done?", default: false }
-  }
-}
-
-function Edit() {
+function Edit(props) {
   return (
     <Form
-      schema={schema}
+      schema={props.schema}
       onChange={log("changed")}
       onSubmit={log("submitted")}
       onError={log("errors")}
     />
   )
+}
+
+Edit.getInitialProps = async function() {
+  const schema = await fetchSchema("task")
+  return { schema }
 }
 
 export default Edit
