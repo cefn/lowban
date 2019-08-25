91d95ef9e40c579a338b7d59d3c0c27a1ea4d86d
Cefn Hoile <github.com@cefn.com>
2019-08-25 10:01:38 +0100

    Add submit logic

--- pages/edit.js
+++ pages/edit.js
@@ -2,9 +2,10 @@ import React, { Component } from "react"
 import { render } from "react-dom"
 import Form from "react-jsonschema-form"
 const fetch = require("isomorphic-unfetch")
+const { host } = require("../defaults")
 
 async function fetchSchema(typeName) {
-  const endpoint = `http://localhost:3000/schema/${typeName}`
+  const endpoint = `${host}/schema/${typeName}`
   const res = await fetch(endpoint, {
     method: "GET",
     headers: {
@@ -16,13 +17,39 @@ async function fetchSchema(typeName) {
 
 const log = type => console.log.bind(console, type)
 
+function submit({ formData, schema }, e) {
+  console.log("schemaData: " + JSON.stringify(schema))
+  console.log("formData: " + JSON.stringify(formData))
+  const fetch = require("isomorphic-unfetch")
+
+  return fetch(`${host}/graphql`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      query: `mutation ($submitted:TaskInput!){
+            saveEditedTask(input:$submitted){
+                id
+            }
+        }`,
+      variables: {
+        submitted: formData
+      }
+    })
+  })
+    .then(r => r.json())
+    .then(data => console.log("data returned:", data))
+}
+
 function Edit(props) {
   return (
     <Form
       schema={props.schema}
       uiSchema={props.uiSchema}
       onChange={log("changed")}
-      onSubmit={log("submitted")}
+      onSubmit={submit}
       onError={log("errors")}
     />
   )
@@ -32,7 +59,10 @@ Edit.getInitialProps = async function() {
   const schema = await fetchSchema("task")
   let uiSchema = {
     note: {
-      "ui:widget": "textarea"
+      "ui:widget": "textarea",
+      "ui:options": {
+        rows: 4
+      }
     }
   }
   const order = schema["$order"]
