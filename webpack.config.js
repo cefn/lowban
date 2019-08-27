18e0029937fdc0d385134dc8b5221cd7942999a5
Cefn Hoile <github.com@cefn.com>
2019-08-27 01:43:03 +0100

    Tooling config to accommodate babel and client-side js

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path")
+const { mode } = require("./defaults")
+
+module.exports = {
+    mode: mode,
+    entry: {
+        "main": "index.js"
+    },
+    output: {
+        path: path.join(__dirname, "/static/build/"),
+        filename: "index.js",
+        publicPath: "/static/build/"
+    }
+    ,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: {
+        modules: [__dirname, "node_modules"],
+    }
+}
