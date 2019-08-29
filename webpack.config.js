ba1a2475f288964c9fb15ea27fe9286c92934087
Cefn Hoile <github.com@cefn.com>
2019-08-30 00:03:27 +0100

    Added source map support. Linted whitespace

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,31 @@ const path = require("path")
 const { mode } = require("./defaults")
 
 module.exports = {
-    mode: mode,
-    entry: {
-        "main": "index.js"
-    },
-    output: {
-        path: path.join(__dirname, "/static/build/"),
-        filename: "index.js",
-        publicPath: "/static/build/"
-    }
-    ,
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ["babel-loader"]
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    },
-    resolve: {
-        modules: [__dirname, "node_modules"],
-    }
+  mode: mode,
+  devtool: "eval-source-map",
+  entry: {
+    "main": "index.js"
+  },
+  output: {
+    path: path.join(__dirname, "/static/build/"),
+    filename: "index.js",
+    publicPath: "/static/build/"
+  }
+  ,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: {
+    modules: [__dirname, "node_modules"],
+  }
 }
