3daa154e43cbdbd186168e9af0c2228ca5313145
Cefn Hoile <github.com@cefn.com>
2019-09-22 13:03:31 +0100

    Debugged and improved test coverage for tagstore

--- test/unit/tagstore.test.js
+++ test/unit/tagstore.test.js
@@ -1,8 +1,8 @@
 const _ = require("lodash")
 const { dbInMemory } = require("../../lib/lowstore")
 const {
-  iterateStoredTypes,
-  iterateTraversableTypes
+  storedDataTypes,
+  listedDataTypes
 } = require("../../lib/tagmodel")
 const { createDefaultTree, TagStore } = require("../../lib/tagstore")
 
@@ -16,17 +16,14 @@ function mockTagStore(tree) {
 
 test("Default data conforms to db structure", () => {
   const defaultTree = createDefaultTree()
-  const typeNames = [...iterateStoredTypes()]
   for (const [typeName, typeCollection] of Object.entries(defaultTree)) {
-    typeNames.includes(typeName)
+    storedDataTypes.includes(typeName)
     Array.isArray(typeCollection)
   }
 })
 
 test("tags are traversable even though not stored", () => {
-  const storedTypes = [...iterateStoredTypes()]
-  const traversableTypes = [...iterateTraversableTypes()]
-  expect(_.difference(traversableTypes, storedTypes)).toEqual(["tag"])
+  expect(_.difference(listedDataTypes, storedDataTypes)).toEqual(["tag"])
 })
 
 test("lazyCreateTag populates tag when not in database", () => {
@@ -38,7 +35,7 @@ test("lazyCreateTag populates tag when not in database", () => {
 test("lazyCreateTag loads tag when found in database", () => {
   const storedTagRecord = {
     id: "!superimportant",
-    title: "Level of Priority",
+    label: "Level of Priority",
     content: "More detail if you needed"
   }
   const tagStore = mockTagStore({ priority: [storedTagRecord] })
@@ -51,7 +48,7 @@ test("iterateAllTagIds includes ids from declared tags and transient tags", () =
     task: [
       {
         id: "0",
-        title: "Name",
+        label: "Name",
         tagIds: [
           "transientWhat",
           "!transientWhether",
@@ -60,10 +57,10 @@ test("iterateAllTagIds includes ids from declared tags and transient tags", () =
         ]
       }
     ],
-    category: [{ id: "what", title: "Some Category" }],
-    priority: [{ id: "!whether", title: "Some Priority" }],
-    context: [{ id: "@where", title: "Some Context" }],
-    status: [{ id: "#which", title: "Some Status" }]
+    category: [{ id: "what", label: "Some Category" }],
+    priority: [{ id: "!whether", label: "Some Priority" }],
+    context: [{ id: "@where", label: "Some Context" }],
+    status: [{ id: "#which", label: "Some Status" }]
   })
   const tags = [...tagStore.iterateAllTagIds()]
   const actual = new Set(tags)
@@ -96,14 +93,14 @@ test("iterateAllTags includes tags from declared tags and transient tags", () =>
     task: [
       {
         id: "0",
-        title: "Name",
+        label: "Name",
         tagIds: ["transientTag"]
       }
     ],
-    category: [{ id: "what", title: "Some Category" }],
-    priority: [{ id: "!whether", title: "Some Priority" }],
-    context: [{ id: "@where", title: "Some Context" }],
-    status: [{ id: "#which", title: "Some Status" }]
+    category: [{ id: "what", label: "Some Category" }],
+    priority: [{ id: "!whether", label: "Some Priority" }],
+    context: [{ id: "@where", label: "Some Context" }],
+    status: [{ id: "#which", label: "Some Status" }]
   })
   const tags = [...tagStore.iterateAllTags()]
   const tagIds = _.map(tags, "id")
@@ -123,25 +120,117 @@ test("iterateTaskTags retrieve a task's declared tags, constructs transient tags
     task: [
       {
         id: "0",
-        title: "Feed the Cat",
+        label: "Feed the Cat",
         content: "Leftover chicken in fridge",
-        tagIds: ["!critical", "@home", "cat", "freshmeat"]
+        tagIds: ["!urgent", "@home", "cat", "freshmeat"]
       }
     ],
     category: [
-      { id: "cat", title: "The Beast", content: "Our lovely cat, William" }
+      { id: "cat", label: "The Beast", content: "Our lovely cat, William" }
     ],
-    priority: [{ id: "!critical", title: "Must Do" }],
-    context: [{ id: "@home", title: "Stuff I can do in the house" }]
+    priority: [{ id: "!urgent", label: "Must Do" }],
+    context: [{ id: "@home", label: "Stuff I can do in the house" }]
   })
   const task = tagStore.getById("task", "0")
   const tags = tagStore.iterateTaskTags(task)
   const actualSet = new Set(tags)
   const expectedSet = new Set([
-    { id: "cat", title: "The Beast", content: "Our lovely cat, William" },
-    { id: "!critical", title: "Must Do" },
-    { id: "@home", title: "Stuff I can do in the house" },
+    { id: "cat", label: "The Beast", content: "Our lovely cat, William" },
+    { id: "!urgent", label: "Must Do" },
+    { id: "@home", label: "Stuff I can do in the house" },
     { id: "freshmeat" }
   ])
   expect(actualSet).toEqual(expectedSet)
 })
+
+test("iterateFilteredTasks() returns tasks with tag matches", () => {
+  const tagStore = mockTagStore({
+    task: [
+      {
+        id: "cat",
+        label: "Feed the Cat",
+        tagIds: ["!urgent", "@home", "cat"]
+      }
+    ],
+  })
+  expect([...tagStore.iterateFilteredTasks("cat")].map((task) => task.id)).toEqual(["cat"])
+  expect([...tagStore.iterateFilteredTasks("home")].map((task) => task.id)).toEqual(["cat"])
+  expect([...tagStore.iterateFilteredTasks("urgent")].map((task) => task.id)).toEqual(["cat"])
+  expect([...tagStore.iterateFilteredTasks("@home")].map((task) => task.id)).toEqual(["cat"])
+  expect([...tagStore.iterateFilteredTasks("!urgent")].map((task) => task.id)).toEqual(["cat"])
+})
+
+test("iterateFilteredTasks() returns tasks by case-insensitive content match", () => {
+  const tagStore = mockTagStore({
+    task: [
+      { id: "cat", label: "Give dry food to William", tagIds: ["!urgent", "@home", "cat"] },
+      { id: "dog", label: "Give canned food to Rover", tagIds: ["@home", "dog"] }
+    ],
+  })
+  expect([...tagStore.iterateFilteredTasks("give")].map((task) => task.id)).toEqual(["cat", "dog"])
+  expect([...tagStore.iterateFilteredTasks("food")].map((task) => task.id)).toEqual(["cat", "dog"])
+  expect([...tagStore.iterateFilteredTasks("dry")].map((task) => task.id)).toEqual(["cat"])
+  expect([...tagStore.iterateFilteredTasks("canned")].map((task) => task.id)).toEqual(["dog"])
+})
+
+
+test("iterateFilteredTasks() treats terms with tag prefixes as tags only ", () => {
+  const tagStore = mockTagStore({
+    task: [
+      { id: "cat", label: "Give dry food to William", tagIds: ["!urgent", "@home", "cat"] },
+      { id: "dog", label: "Give canned food to Rover with tag !urgent", tagIds: ["@home", "dog"] }
+    ],
+  })
+  expect([...tagStore.iterateFilteredTasks("urgent")].map((task) => task.id)).toEqual(["cat", "dog"])
+  expect([...tagStore.iterateFilteredTasks("!urgent")].map((task) => task.id)).toEqual(["cat"])
+})
+
+test("randomItem() returns sampled item", () => {
+  const tagStore = mockTagStore({
+    task: [
+      { id: "cat", label: "SameLabel" },
+      { id: "dog", label: "SameLabel" }
+    ],
+    category: [{ id: "garden", label: "Stuff for the yard" }],
+    status: [],
+  })
+  const sampledTask = tagStore.randomItem("task")
+  expect(sampledTask).toHaveProperty("label")
+  expect(sampledTask.label).toBe("SameLabel")
+
+  const sampledCategory = tagStore.randomItem("category")
+  expect(sampledCategory.id).toBe("garden")
+
+  const sampledStatus = tagStore.randomItem("status")
+  expect(sampledStatus).toBe(null)
+
+})
+
+test("iterateFilteredTags() returns tags with id matching search term", () => {
+  const tagStore = mockTagStore({
+    task: [
+      { id: "cat", label: "Give dry food to William", tagIds: ["!urgent", "@home", "cat"] },
+    ],
+    category: [{ id: "cat", label: "Stuff for William" }]
+  })
+  expect([...tagStore.iterateFilteredTags("cat")].map((tag) => tag.label)).toEqual(["Stuff for William"])
+  expect([...tagStore.iterateFilteredTags("ome")].map((tag) => tag.id)).toEqual(["@home"])
+})
+
+
+// test("iterateActionableTasks() excludes one-time tasks containing a fulfil action", () => {
+//   const tagStore = mockTagStore({
+//     task: [
+//       {
+//         label: "Done",
+//         action: [
+//           { type: "fulfil" }
+//         ]
+//       },
+//       { label: "Undone" },
+//     ]
+//   })
+//   const actual = [...tagStore.iterateActionableTasks()]
+//   const expected = [{ label: "Undone" }]
+//   expect(actual).toEqual(expected)
+// })
