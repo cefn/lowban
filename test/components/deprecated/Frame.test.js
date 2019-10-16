bdd23645b148f6d29332ef7209c5671fbba44326
Cefn Hoile <github.com@cefn.com>
2019-10-16 18:56:41 +0100

    Moved files

--- test/components/deprecated/Frame.test.js
+++ test/components/deprecated/Frame.test.js
@@ -3,7 +3,7 @@ const {
   iterateParamNames,
   iteratePatternFragments,
   createParamExtractor,
-} = require("../../../../components/deprecated/Frame")
+} = require("../../../components/deprecated/Frame")
 
 test("Names can be extracted from a path pattern", () => {
   const pathPattern = "/list/:type/:filter"
