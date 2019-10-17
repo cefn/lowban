9a68111c1767b37823bf90bb6f6b8d45285c5e80
Cefn Hoile <github.com@cefn.com>
2019-10-17 23:36:42 +0100

    Working tests for backend

---
+++ test/client/backend.test.js
@@ -0,0 +1,69 @@
+const backend = require("../../client/backend")
+
+const { launchServer } = require("../../server")
+const { dbInMemory } = require("../../lib/lowstore")
+const { createDefaultTree } = require("../../lib/tagstore")
+
+function mockDb(tree) {
+  if (!tree) {
+    tree = createDefaultTree()
+  }
+  return dbInMemory(tree)
+}
+
+function withServerStore(...args) {
+  let tree, fn
+  if (args.length == 1) { //handle single arg as fn (tree undefined)
+    [fn] = args
+    tree = undefined
+  }
+  else {
+    [tree, fn] = args
+  }
+
+  return async () => {
+    let handle, store
+    try {
+      const db = mockDb(tree);
+      [handle, store] = launchServer(db)
+      return await fn(store)
+    }
+    finally {
+      if (handle) handle.close()
+    }
+  }
+}
+
+it("Can load a schema", withServerStore(async () => {
+  const taskSchema = await backend.loadSchema("task")
+  expect(taskSchema).toHaveProperty("$id", "https://github.com/cefn/lowban/tree/v0.2.0/schema/task.schema.json")
+}))
+
+it("Can list ids by item type", withServerStore({ task: [{ id: "a" }, { id: "b" }, { id: "c" }] }, async () => {
+  const itemType = "task"
+  const ids = await backend.loadIds(itemType)
+  expect(ids).toEqual(["a", "b", "c"])
+}))
+
+it("Can load an item by type and id", withServerStore({ task: [{ id: "a", label: "Ape" }, { id: "b", label: "Bee" }, { id: "c", label: "Cat" }] }, async () => {
+  const itemType = "task"
+  const itemId = "b"
+  const item = await backend.loadItem(itemType, itemId)
+  expect(item.label).toEqual("Bee")
+}))
+
+it("Can save item by type, attaching id", withServerStore(async (db) => {
+  const itemType = "task"
+  const sentItem = { label: "Zebra" }
+  const receivedItem = await backend.saveItem(itemType, sentItem)
+  const receivedId = receivedItem.id
+  //id was assigned to record by server
+  expect(receivedId).not.toBeNull()
+  //remaining fields were returned in server response (TODO check for all fields in type?)
+  expect(receivedItem.label).toBe("Zebra")
+  //record was actually stored in DB
+  const record = db.queryTable("task", table => table.getById(receivedId))
+  expect(record.label).toBe("Zebra")
+}))
+
+
