9d130ec2764f95c765ab462549f6c716b37b7469
Cefn Hoile <github.com@cefn.com>
2019-10-18 14:59:12 +0100

    Simplified all cases of lazy population of a state path in the store. Exported all symbols for testing. Made selectorChange monitor change by deep equals

--- redux/saga.js
+++ redux/saga.js
@@ -1,11 +1,16 @@
 /* eslint-disable require-atomic-updates */
+const isEqual = require("lodash/isEqual")
 const { take, select, call, put, spawn } = require("redux-saga/effects")
-const { setPathsAction } = require("../redux/store")
+
+const { getPathSelector } = require("../lib/util/selector")
+
+const { setPathsAction, createSagaPathStore } = require("../redux/store")
+const { defaultState, getSchemaPath, getRowPath } = require("./state")
 const backend = require("../client/backend")
 
-function* monitorSelector(selector, valueFilter, takePattern = "*") {
+function* monitorSelector(valueSelector, valueFilter, takePattern = "*") {
   while (true) {
-    const nextValue = yield select(selector)
+    const nextValue = yield select(valueSelector)
     if (valueFilter(nextValue)) {
       return nextValue
     }
@@ -16,34 +21,72 @@ function* monitorSelector(selector, valueFilter, takePattern = "*") {
 function* selectorChangeSaga(selector, saga) {
   let previous
   while (true) {
-    const next = yield* monitorSelector(selector, next => next !== previous)
+    const next = yield* monitorSelector(selector, next => !isEqual(next, previous))
     yield* saga(next, previous)
     previous = next
   }
 }
 
-function* ensureSchemaLoaded() {
-  console.error("entered ensureSchemaLoaded")
+function* populate(path, ...invocation) {
+  const next = yield call(...invocation)
+  yield put(setPathsAction({ [path]: next }))
+}
+
+function* lazyPopulate(path, ...invocation) {
+  const previous = yield select(getPathSelector(path))
+  if (previous) {
+    return
+  }
+  else {
+    yield* populate(path, ...invocation)
+  }
+}
+
+function* loadSchema(type) {
+  yield* lazyPopulate(getSchemaPath(type), backend.loadSchema, type)
+}
+
+function* loadRow(type, id) {
+  yield* lazyPopulate(getRowPath(type, id), backend.loadItem, type, id)
+}
+
+function* ensureFocusSchemaLoaded() {
   yield* selectorChangeSaga(state => state.focusType, function* (focusType) {
-    console.error(`focusType is ${focusType}`)
     if (focusType) {
-      let loadedSchema = yield select(state => state.schemas[focusType])
-      console.error(`loadedSchema is ${loadedSchema}`)
-      if (!loadedSchema) {
-        loadedSchema = yield call(backend.loadSchema, focusType)
-        console.error(`retrieved ${loadedSchema}`)
-        yield put(setPathsAction({ [`schemas.${focusType}`]: loadedSchema }))
-        console.error(`put loadedSchema ${loadedSchema}`)
-      }
+      yield* loadSchema(focusType)
+    }
+  })
+}
+
+function* ensureFocusRowLoaded() {
+  yield* selectorChangeSaga(state => [state.focusType, state.focusId], function* ([focusType, focusId]) {
+    if (focusType && focusId) {
+      yield* loadRow(focusType, focusId)
     }
   })
 }
 
 function* rootSaga() {
-  yield spawn(ensureSchemaLoaded)
+  yield spawn(ensureFocusSchemaLoaded)
+}
+
+function launchRootSaga(initialState = defaultState) {
+  const [reduxStore, sagaMiddleware] = createSagaPathStore(initialState)
+  sagaMiddleware.run(rootSaga)
+  return [reduxStore, sagaMiddleware]
 }
 
 module.exports = {
-  ensureSchemaLoaded,
-  rootSaga
+  testing: {
+    monitorSelector,
+    selectorChangeSaga,
+    populate,
+    lazyPopulate,
+    loadSchema,
+    loadRow,
+    ensureFocusSchemaLoaded,
+    ensureFocusRowLoaded,
+    rootSaga,
+  },
+  launchRootSaga
 }
