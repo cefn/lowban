91eb4868a8987810ef127ee2d2f76596d51fabfe
Cefn Hoile <github.com@cefn.com>
2019-10-27 08:02:28 +0000

    Added focusAction, listPath

--- domain/todo/redux/action.js
+++ domain/todo/redux/action.js
@@ -1,15 +1,25 @@
+const { setPathsAction } = require("../../../lib/util/redux/path")
+
 const SAVE_ITEM = "save-item"
 
-function saveItemAction(itemType, item) {
+function saveItemAction(type, item) {
   return {
     type: SAVE_ITEM,
     payload: {
-      itemType,
+      type,
       item
     }
   }
 }
 
+function focusAction(focusType, focusId) {
+  return setPathsAction({
+    focusType,
+    focusId,
+  })
+}
+
 module.exports = {
-  saveItemAction
+  saveItemAction,
+  focusAction,
 }
--- domain/todo/redux/store.js
+++ domain/todo/redux/store.js
@@ -3,11 +3,12 @@ const { storedDataTypes } = require("../tagmodel")
 const defaultState = {
   types: [...storedDataTypes], //list of types
   schemas: {}, //schemas by type
-  ids: {}, //id lists by type
   //rows mapped by type, then by id (as defined by normalizr)
   rows: Object.assign({}, ...storedDataTypes.map(typeName => ({ [typeName]: [] }))),
+  lists: {}, // lists by type, listname
   focusType: null, //the row type in focus
   focusId: null, //the row id in focus
+  focusString: "", //the string filtering views
 }
 
 function getRowPath(type, id) {
@@ -18,6 +19,10 @@ function getSchemaPath(type) {
   return `schemas.${type}`
 }
 
+function getListPath(name) {
+  return `lists.${name}`
+}
+
 function focusSelector(state) {
   return [state.focusType, state.focusId]
 }
@@ -26,5 +31,6 @@ module.exports = {
   defaultState,
   getRowPath,
   getSchemaPath,
+  getListPath,
   focusSelector
 }
