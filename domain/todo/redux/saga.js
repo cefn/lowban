eff22a97673e17babe2aa8f3589e003c063ece68
Cefn Hoile <github.com@cefn.com>
2019-10-27 08:02:05 +0000

    Added lists loading

--- domain/todo/redux/saga.js
+++ domain/todo/redux/saga.js
@@ -1,23 +1,30 @@
-const { spawn } = require("redux-saga/effects")
-
-const { launchStore } = require("./store")
-const { defaultState, getSchemaPath, getRowPath } = require("./store")
-const { lazyPopulatePathSaga, } = require("../../../lib/util/redux/path")
+const { spawn, getContext, setContext } = require("redux-saga/effects")
+const { launchPathStore } = require("../../../lib/util/redux/path")
+const { defaultState, getSchemaPath, getRowPath, getListPath } = require("./store")
+const { populatePathSaga, lazyPopulatePathSaga, } = require("../../../lib/util/redux/path")
 const { selectorChangeSaga } = require("../../../lib/util/redux/watch")
-const backend = require("../../../client/backend")
+const { createBackend } = require("../../../client/backend")
+const { host } = require("../../../server/defaults")
 
-function* loadSchema(type) {
+function* loadSchemaSaga(type) {
+  const backend = yield getContext("backend")
   yield* lazyPopulatePathSaga(getSchemaPath(type), backend.loadSchema, type)
 }
 
-function* loadRow(type, id) {
+function* loadRowSaga(type, id) {
+  const backend = yield getContext("backend")
   yield* lazyPopulatePathSaga(getRowPath(type, id), backend.loadItem, type, id)
+}
+
+function* loadListSaga(listName, listArgs = null, listFields = ["id", "label"]) {
+  const backend = yield getContext("backend")
+  yield* populatePathSaga(getListPath(listName), backend.loadList, listName, listArgs, listFields)
 }
 
 function* ensureFocusSchemaLoaded() {
   yield* selectorChangeSaga(state => state.focusType, function* (focusType) {
     if (focusType) {
-      yield* loadSchema(focusType)
+      yield* loadSchemaSaga(focusType)
     }
   })
 }
@@ -25,27 +32,44 @@ function* ensureFocusSchemaLoaded() {
 function* ensureFocusRowLoaded() {
   yield* selectorChangeSaga(state => [state.focusType, state.focusId], function* ([focusType, focusId]) {
     if (focusType && focusId) {
-      yield* loadRow(focusType, focusId)
+      yield* loadRowSaga(focusType, focusId)
     }
+  })
+}
+
+function* ensureFilterTaskListLoaded() {
+  yield* selectorChangeSaga(state => state.focusString, function* (focusString) {
+    focusString = focusString || ""
+    yield* loadListSaga("filterTask", { filter: focusString })
   })
 }
 
 function* rootSaga() {
   yield spawn(ensureFocusSchemaLoaded)
   yield spawn(ensureFocusRowLoaded)
+  yield spawn(ensureFilterTaskListLoaded)
 }
 
 function launchRootSaga(initialState = defaultState) {
-  const [reduxStore, sagaMiddleware] = launchStore(initialState)
-  const task = sagaMiddleware.run(rootSaga)
-  return [reduxStore, sagaMiddleware, task]
+  const backend = createBackend(host)
+  const context = { backend }
+  const [reduxStore, sagaMiddleware] = launchPathStore(initialState, context)
+  const rootTask = sagaMiddleware.run(rootSaga)
+  return {
+    reduxStore,
+    sagaMiddleware,
+    backend,
+    rootTask
+  }
 }
 
 module.exports = {
-  loadSchema,
-  loadRow,
+  loadSchemaSaga,
+  loadRowSaga,
+  loadListSaga,
   ensureFocusSchemaLoaded,
   ensureFocusRowLoaded,
+  ensureFilterTaskListLoaded,
   rootSaga,
   launchRootSaga
 }
