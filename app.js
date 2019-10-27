1233b252ea804a5c578b2e82f9bf58d2ff00ba55
Cefn Hoile <github.com@cefn.com>
2019-10-27 08:07:49 +0000

    Renamed launch function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const path = require("path")
 const { dbFromPath } = require("./lib/lowstore")
-const { hostDb: launchServer } = require("./server/host")
+const { hostDb } = require("./server/host")
 
 const db = dbFromPath(path.join(__dirname, "db.json"))
-launchServer(db)
+hostDb(db)
