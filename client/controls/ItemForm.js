5bc1833292ed9dbc86f4f69d966fdf12713a4370
Cefn Hoile <github.com@cefn.com>
2019-11-02 14:59:25 +0000

    Simplified ItemForm to allow item and its fields to be undefined. Force width of fields.

--- client/controls/ItemForm.js
+++ client/controls/ItemForm.js
@@ -9,15 +9,15 @@ const {
 
 function ItemForm({ item, fieldNames, areaFieldNames, dirty }) {
 
-  for (const name of fieldNames) {
-    item[name] = item[name] || ""
+  if (!item) {
+    return <h1>Waiting to load item</h1>
   }
 
   function createFieldChangeHandler(fieldName) {
     return (event) => {
       //note this has to be set in the closure before setEditedItem
       Object.assign(item, { [fieldName]: event.target.value })
-      dirty()
+      dirty(item)
     }
   }
 
@@ -25,8 +25,9 @@ function ItemForm({ item, fieldNames, areaFieldNames, dirty }) {
     {fieldNames.map((fieldName) => {
       let props = {
         label: fieldName,
-        value: item[fieldName],
-        onChange: createFieldChangeHandler(fieldName)
+        value: item[fieldName] || "",
+        onChange: createFieldChangeHandler(fieldName),
+        style: { width: "100%" },
       }
       if (areaFieldNames && areaFieldNames.includes(fieldName)) { //use a text area
         return <ListItem key={fieldName} > <TextareaAutosize {...props} rows={10} /> </ListItem>
