51a8b27e2f1c30702e223d6fc24d15f634c7f1a9
Cefn Hoile <github.com@cefn.com>
2019-11-03 20:35:38 +0000

    Archived old component hierarchy

--- components/index.js
+++
@@ -1,8 +0,0 @@
-import "bootstrap"
-import "bootstrap/dist/css/bootstrap.min.css"
-
-import React from "react"
-import ReactDom from "react-dom"
-import Dash from "components/Dash"
-
-ReactDom.render(<Dash />, document.getElementById("app"))
