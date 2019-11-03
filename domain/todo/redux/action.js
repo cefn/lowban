6ce5c664945089999905ec0c9d9aed6d461c8216
Cefn Hoile <github.com@cefn.com>
2019-11-03 15:37:45 +0000

    Added draft user actions for task manipulation

--- domain/todo/redux/action.js
+++ domain/todo/redux/action.js
@@ -1,15 +1,47 @@
 const { setPathsAction } = require("../../../lib/util/redux/path")
 
+const DELETE_ITEM = "delete-item"
+const SNOOZE_TASK = "snooze-task"
+const FULFIL_TASK = "fulfil-task"
 
-/** USER-INITIATED ACTIONS */
+/** SAGA-HANDLED ACTIONS */
+
+function deleteItemAction(type, id) {
+  return {
+    type: DELETE_ITEM,
+    payload: {
+      type,
+      id
+    }
+  }
+}
+
+function snoozeTaskAction(id, until) {
+  return {
+    type: SNOOZE_TASK,
+    payload: {
+      id,
+      until
+    }
+  }
+}
+
+function fulfilTaskAction(id) {
+  return {
+    type: FULFIL_TASK,
+    payload: {
+      id
+    }
+  }
+}
+
+/** STORE MANIPULATION ACTIONS */
 
 function saveItemAction(item) {
   return setPathsAction({
     "editor.item": item
   })
 }
-
-/** STORE MANIPULATION ACTIONS */
 
 function setEditedAction(type, id) {
   return setPathsAction({
@@ -31,6 +63,9 @@ function filterTasksAction(taskFilterString) {
 }
 
 module.exports = {
+  deleteItemAction,
+  snoozeTaskAction,
+  fulfilTaskAction,
   saveItemAction,
   setEditedAction,
   filterTagsAction,
