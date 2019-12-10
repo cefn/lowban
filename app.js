ad4cc0d893d4971f36c4b07f820ef94c15d6e156
Cefn Hoile <cefn.hoile@bbc.co.uk>
2019-12-10 08:00:48 +0000

    Optionally specify db file in argv. Assert node version

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ const path = require("path")
 const { dbFromPath } = require("./lib/lowstore")
 const { hostDb } = require("./server/host")
 
-const db = dbFromPath(path.join(__dirname, "db.json"))
+const dbPath = process.argv.length > 2 ?
+  process.argv[2] :
+  path.join(__dirname, "db.json")
+
+const db = dbFromPath(dbPath)
 hostDb(db)
